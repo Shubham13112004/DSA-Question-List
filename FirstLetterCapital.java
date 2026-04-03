e5bccb6f6d8cad61d4cc86b9a6948a34b69e9e12
Shubham13112004 <desaishubham696@gmail.com>
2026-04-03 14:28:14 +0530

    Daily Practice

--- FirstLetterCapital.java
+++ FirstLetterCapital.java
@@ -1,3 +1,21 @@
 public class FirstLetterCapital {
-    
+    public static void main(String[] args) {
+        String str = "hello world in this siddhesh you me i am";
+
+        String[] s = str.split(" ");
+        String b = "";
+
+        for(int i = 0;i<s.length;i++){
+            String word = s[i];
+            String first = word.substring(0,1).toUpperCase();
+            String rest = first+word.substring(1);
+
+             b +=rest+" ";
+            
+        }
+
+        System.out.println(b);
+
+
+    }
 }
